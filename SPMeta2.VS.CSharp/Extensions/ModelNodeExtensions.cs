9bf641be15cbbd59157c36718f5bcdf4907b0c45
support@subpointsolutions.com <support@subpointsolutions.com>
2015-08-14 19:39:36 +1000

    added missed packages, VSPackage extension POC

--- SPMeta2.VS.CSharp/Extensions/ModelNodeExtensions.cs
+++ SPMeta2.VS.CSharp/Extensions/ModelNodeExtensions.cs
@@ -8,5 +8,27 @@ namespace SPMeta2.VS.CSharp.Extensions
 {
     public static class ModelNodeExtensions
     {
+        #region methods
+        public static SiteModelNode $M2PrjPrefix$ImportFields(this SiteModelNode node, IEnumerable<FieldDefinition> fields)
+        {
+            foreach (var field in fields)
+                node.AddField(node);
+
+            return node;
+        } 
+
+        public static ListModelNode $M2PrjPrefix$SetDefaultContentType(this ListModelNode node, ContentTypeDefinition contentType)
+        {
+           return $M2PrjPrefix$SetDefaultContentType(node, contentType.Name);
+        }
+ 
+        public static ListModelNode $M2PrjPrefix$SetDefaultContentType(this ListModelNode node, string contentTypeName)
+        {
+           //node.AddUniqueContentTypeFieldOrder(new )
+
+           return node;
+        }
+
+        #endregion
     }
 }
---
+++ SPMeta2.VS.Tooling/Packages/M2Package.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace SPMeta2.VS.Tooling.Packages
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("M2 Extensions", "", "3.0")]
+    [ProvideAutoLoad("{f1536ef8-92ec-443c-9ed7-fdadf150da82}")]
+    //[ProvideAutoLoad(Microsoft.VisualStudio.VSConstants.UICONTEXT.NoSolution_string)]
+    [Guid("CBBB8ABF-B5EA-460E-94CB-5757D2C885F9")]
+
+    public sealed class M2Package : Package
+    {
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public M2Package()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+        }
+
+
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            base.Initialize();
+
+        }
+        #endregion
+
+    }
+}
