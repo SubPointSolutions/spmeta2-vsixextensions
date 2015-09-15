90a4620da9bb8c5396d9b6c66a4b689dbb2426e0
support@subpointsolutions.com <support@subpointsolutions.com>
2015-09-15 23:05:15 +1000

    added M2-project-templates images

--- SPMeta2.VsixExtensions/Properties/AssemblyInfo.cs
+++ SPMeta2.VsixExtensions/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.15257.1343")]
+[assembly: AssemblyFileVersion("1.0.15258.1132")]
