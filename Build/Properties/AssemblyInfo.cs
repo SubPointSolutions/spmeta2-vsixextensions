d9ab5f5eb2b86c08eb39d96d81f66cbe9442d727
support@subpointsolutions.com <support@subpointsolutions.com>
2017-02-20 22:44:19 +0000

    + build fix

--- Build/Properties/AssemblyInfo.cs
+++ Build/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Build")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vagrant")]
 [assembly: AssemblyProduct("Build")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Vagrant 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6fdc1d75-7c3c-421e-81db-e84505545b6f")]
+[assembly: Guid("65487559-a5dc-42dc-8f26-d187429088d4")]
 
 // Version information for an assembly consists of the following four values:
 //
